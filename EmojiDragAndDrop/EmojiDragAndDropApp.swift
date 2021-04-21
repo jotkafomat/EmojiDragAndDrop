e255bd664aecfba1ed21a84eb57d7ced634787e2
jotkafomat <k-j@xl.wp.pl>
2021-04-21 10:50:55 +0100

    Initial Commit

---
+++ EmojiDragAndDrop/EmojiDragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiDragAndDropApp.swift
+//  EmojiDragAndDrop
+//
+//  Created by Krzysztof Jankowski on 21/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiDragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
