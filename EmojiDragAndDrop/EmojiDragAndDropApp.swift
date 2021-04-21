74971d472d1f60f465ab1603ec8431a164fd1efb
jotkafomat <k-j@xl.wp.pl>
2021-04-21 11:02:49 +0100

    add default palette and backgroundImage

--- EmojiDragAndDrop/EmojiDragAndDropApp.swift
+++ EmojiDragAndDrop/EmojiDragAndDropApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EmojiDragAndDropApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmojiArtDocumentView(document: EmojiArtDocument())
         }
     }
 }
