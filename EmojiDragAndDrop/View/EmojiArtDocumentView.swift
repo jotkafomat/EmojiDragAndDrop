0f96b32bfa2d6a4727d53a9242d4579158d77701
jotkafomat <k-j@xl.wp.pl>
2021-04-22 15:00:38 +0100

    add doublet gesture to zoom to fit

--- EmojiDragAndDrop/View/EmojiArtDocumentView.swift
+++ EmojiDragAndDrop/View/EmojiArtDocumentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct EmojiArtDocumentView: View {
     @ObservedObject var document: EmojiArtDocument
     
+    
     var body: some View {
         VStack {
             ScrollView(.horizontal) {
@@ -26,10 +27,13 @@ struct EmojiArtDocumentView: View {
                 ZStack {
                     Color.white.overlay(
                         OptionalImage(uiImage: document.backgroundImage)
+                            .scaleEffect(zoomScale)
                     )
+                    .gesture(doubleTabToZoom(in: geometry.size))
+                    
                     ForEach(document.emojis) { emoji in
                         Text(emoji.text)
-                            .font(font(for: emoji))
+                            .font(animatableWithSize: emoji.fontSize * zoomScale)
                             .position(position(for: emoji, in: geometry.size))
                     }
                 }
@@ -38,18 +42,43 @@ struct EmojiArtDocumentView: View {
                 .onDrop(of: ["public.image","public.text"], isTargeted: nil) { providers, location in
                     var location = CGPoint(x: location.x, y: geometry.convert(location, from: .global).y)
                     location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
+                    location = CGPoint(x: location.x / zoomScale, y: location.y / zoomScale)
                     return self.drop(providers: providers, at: location)
                 }
             }
         }
     }
+    //    DoubleTap Gesture
+    @State var zoomScale: CGFloat = 1.0
+    
+    private func zoomToFit(_ image: UIImage?, size: CGSize) {
+        if let image = image, image.size.width > 0, image.size.height > 0 {
+            let hZoom = size.width / image.size.width
+            let vZoom = size.height / image.size.height
+            zoomScale = min(hZoom, vZoom)
+        }
+    }
+    
+    private func doubleTabToZoom(in size: CGSize) -> some Gesture {
+        TapGesture(count: 2)
+            .onEnded {
+                withAnimation {
+                    zoomToFit(document.backgroundImage, size: size)
+                }
+            }
+    }
+    
+    
     
     private func font(for emoji: EmojiArt.Emoji) -> Font {
-        Font.system(size: emoji.fontSize)
+        Font.system(size: emoji.fontSize * zoomScale)
     }
     
     private func position(for emoji: EmojiArt.Emoji, in size: CGSize) -> CGPoint {
-        CGPoint(x: emoji.location.x + size.width/2, y: emoji.location.y + size.height/2)
+        var location = emoji.location
+        location = CGPoint(x: location.x * zoomScale, y: location.y * zoomScale)
+        location = CGPoint(x: location.x + size.width/2, y: location.y + size.height/2)
+        return location
     }
     
     private func drop(providers: [NSItemProvider], at location: CGPoint) -> Bool {
